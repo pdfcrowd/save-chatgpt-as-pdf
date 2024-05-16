c700ad85d2a5406cf3e9989671808a760c783278
jarda <jgresula@gmail.com>
2024-05-16 08:36:12 +0200

    version fixup

--- tools/check-version.sh
+++ tools/check-version.sh
@@ -7,8 +7,8 @@ VERSION=`grep -Po '"version":\s+"\K[0-9.]+(?=")' manifest.json`
 grep -E "\"version\":\s+\"$VERSION\"" manifest_firefox.json > /dev/null \
     || (echo "VERSION MISMATCH manifest_firefox.json"; false)
 
-grep -E ">v$VERSION<" common.js > /dev/null \
-     || (echo "VERSION MISMATCH: common.js"; false)
+grep -E "'v$VERSION'" shared.js > /dev/null \
+     || (echo "VERSION MISMATCH: shared.js"; false)
 
 grep -E "@version\s+$VERSION" userscript/save-chatgpt-as-pdf.user.js > /dev/null \
      || (echo "VERSION MISMATCH: save-chatgpt-as-pdf.user.js"; false)
