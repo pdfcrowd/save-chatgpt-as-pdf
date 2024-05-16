40e20f8b53a827736e7e380afc54cdb54156c9ec
jarda <jgresula@gmail.com>
2024-05-16 08:36:12 +0200

    version fixup

--- userscript/save-chatgpt-as-pdf.user.js
+++ userscript/save-chatgpt-as-pdf.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Save ChatGPT as PDF
 // @namespace    http://tampermonkey.net/
-// @version      1.9
+// @version      1.10
 // @description  Turn your chats into neatly formatted PDF.
 // @author       PDFCrowd (https://pdfcrowd.com/)
 // @match        https://chatgpt.com/*
