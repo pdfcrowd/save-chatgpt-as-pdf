7df2c2fbb7d881a74ffaef451d2e11ed69cedbf4
jarda <jgresula@gmail.com>
2024-05-22 15:18:09 +0200

    removed popup

--- popup.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('help').innerHTML = pdfcrowdShared.helpContent;
-    document.getElementById('version').innerHTML = pdfcrowdShared.version;
-});
